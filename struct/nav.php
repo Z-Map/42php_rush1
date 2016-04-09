5eb8a52460669c671cd5918ed552b6de1ca2fc84
Sebastien N <snicolet@student.42.fr>
2016-04-09 23:26:56 +0200

    ajout du register

--- struct/nav.php
+++ struct/nav.php
@@ -3,6 +3,6 @@
 		<li><a href="index.php?page=1"><i class="fa fa-home fa-lg"></i> Home</a></li>
 		<li><a href="index.php?page=8"><i class="fa fa-space-shuttle fa-lg"></i> Lol 1 </a></li>
 		<li><a href="index.php?page=8"><i class="fa fa-space-shuttle fa-lg"></i> Lol 2 </a></li>
-		<li><a href="index.php?page=8"><i class="fa fa-space-shuttle fa-lg"></i> Lol 3 </a></li>
+		<li><a href="index.php?page=6"><i class="fa fa-space-shuttle fa-lg"></i>Register</a></li>
 	</ul>
 </nav>

03872a276a0afa1ea1677e16c6888f204177a134
Quentin LOUBIERE <qloubier@e1r7p12.42.fr>
2016-04-09 23:32:27 +0200

    mieux

--- struct/nav.php
+++ struct/nav.php
@@ -1,8 +1,8 @@
 <nav id="mainMenu" >
 	<ul>
 		<li><a href="index.php?page=1"><i class="fa fa-home fa-lg"></i> Home</a></li>
-		<li><a href="index.php?page=8"><i class="fa fa-space-shuttle fa-lg"></i> Lol 1 </a></li>
-		<li><a href="index.php?page=8"><i class="fa fa-space-shuttle fa-lg"></i> Lol 2 </a></li>
-		<li><a href="index.php?page=6"><i class="fa fa-space-shuttle fa-lg"></i>Register</a></li>
+		<li><a href="index.php?page=8"><i class="fa fa-female fa-lg"></i> Lol 1 </a></li>
+		<li><a href="index.php?page=8"><i class="fa fa-female fa-lg"></i> Lol 2 </a></li>
+		<li><a href="index.php?page=6"><i class="fa fa-female fa-lg"></i>Register</a></li>
 	</ul>
 </nav>

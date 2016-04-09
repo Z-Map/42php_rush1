ea94d4c58a54f0d35d64ef9e98184a515c23a317
Quentin LOUBIERE <qloubier@e1r7p12.42.fr>
2016-04-09 21:57:38 +0200

    add css

---
+++ struct/nav.php
@@ -0,0 +1,8 @@
+<nav id="mainMenu" >
+	<a><i class="fa fa-home fa-lg"></i> Home</a>
+	<ul>
+		<li><i class="fa fa-space-shuttle"></i> Lol 1 </li>
+		<li><i class="fa fa-space-shuttle"></i> Lol 2 </li>
+		<li><i class="fa fa-space-shuttle"></i> Lol 3 </li>
+	</ul>
+</nav>

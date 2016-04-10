caf8ede2a9697fcd3373c0625e5091b41beab80c
Sebastien N <snicolet@student.42.fr>
2016-04-10 20:15:47 +0200

    ajout du boutton admin

--- struct/nav.php
+++ struct/nav.php
@@ -1,7 +1,11 @@
 <?php
 	$userline = NULL;
 	if ($_SESSION['user'] !== null)
+	{
 		$userline = "\t\t\t".'<li><a href="./index.php?page=9"><i class="fa fa-eject fa-lg"></i> Logout</a></li>'."\n";
+		if ($_SESSION['user']['login'] === 'admin')
+			$userline .= '<li><a href="./index.php?page=7"><i class="fa fa-usb fa-lg"></i> Admin</a></li>';
+	}
 	else
 		$userline = "\t\t\t".'<li><a href="./index.php?page=10"><i class="fa fa-arrow-circle-o-up fa-lg"></i> Login</a></li>
 		<li><a href="./index.php?page=6"><i class="fa fa-chevron-circle-down fa-lg"></i> Register</a></li>'."\n";

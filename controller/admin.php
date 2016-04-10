a66f4f26bb203dcc0b487d8ce4b4b6265fc7d37c
Sebastien N <snicolet@student.42.fr>
2016-04-10 23:42:44 +0200

    ajout de la section admin

---
+++ controller/admin.php
@@ -0,0 +1,116 @@
+<?php
+	if (krnl_UserIsAdmin())
+	{
+		$db = krnl_MysqlConnect();
+		switch ($_POST["submit"])
+		{
+			case "modify_article":
+				krnl_UpdateArticle($db, $_POST);
+				break;
+			case "remove_article":
+				krnl_RemoveArticle($db, $_POST['id']);
+				break;
+			case "add_article":
+				krnl_AddArticle($db, $_POST);
+				break;
+			case "modify_user":
+
+				break;
+			case "remove_user":
+
+				break;
+			case "modify_category":
+
+				break;
+			case "remove_category":
+
+				break;
+		}
+		$articles = krnl_GetArticles($db);
+		$users = krnl_UserList($db);
+		$categories = krnl_CategoryList($db);
+		mysqli_close($db);
+		?>
+		<div id="admin_page" class="center">
+			<h1>Articles</h1>
+			<div><?php
+				foreach($articles as $item)
+				{
+					?>
+						<form action="index.php" method="POST" class="admin_form">
+							<input placeholder="Titre" type="text" name="title" value="<?php echo $item['title']; ?>">
+							<input placeholder="Prix" type="text" name="price" value="<?php echo $item['price']; ?>">
+							<input placeholder="Lien image" type="text" name="img" value="<?php echo $item['img']; ?>">
+							<input placeholder="Description" type="text" name="description" value="<?php echo $item['description']; ?>">
+							<input placeholder="Categorie" type="text" name="category" value="<?php echo $item['category']; ?>">
+							<input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+							<input type="hidden" name="page" value="7">
+							<button type="submit" name="submit" value="modify_article">Modifier</button>
+							<button type="submit" name="submit" value="remove_article">Supprimer</button>
+						</form>
+					<?php
+				}
+				?>
+				<br>
+				<form action="index.php" method="POST" class="admin_form">
+					<input placeholder="Titre" type="text" name="title">
+					<input placeholder="Prix" type="text" name="price">
+					<input placeholder="Lien image" type="text" name="img">
+					<input placeholder="Description" type="text" name="description">
+					<input placeholder="Categorie" type="text" name="category">
+					<input type="hidden" name="page" value="7">
+					<button type="submit" name="submit" value="add_article">Ajouter</button>
+				</form>
+			</div>
+			<h1>Utilisateurs</h1>
+			<div><?php
+				foreach($users as $user)
+				{
+					?>
+						<form action="index.php" method="POST" class="admin_form">
+							<input placeholder="Login" type="text" name="login" value="<?php echo $user['login']; ?>">
+							<input placeholder="Mail" type="text" name="mail" value="<?php echo $user['mail']; ?>">
+							<input placeholder="Password" type="password" name="password" value="">
+							<input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+							<input type="hidden" name="page" value="7">
+							<button type="submit" name="submit" value="modify_user">Modifier</button>
+							<button type="submit" name="submit" value="remove_user">Supprimer</button>
+						</form>
+					<?php
+				}
+				?>
+			</div>
+			<h1>Categories</h1>
+			<div><?php
+				foreach($categories as $item)
+				{
+					?>
+						<form action="index.php" method="POST" class="admin_form">
+							<input placeholder="Titre" type="text" name="category" value="<?php echo $item['category']; ?>">
+							<input placeholder="Priorité" type="text" name="priority" value="<?php echo $item['priority']; ?>">
+							<input placeholder="Parent" type="text" name="parent" value="<?php echo $item['parent']; ?>">
+							<input placeholder="Lien image" type="text" name="img" value="<?php echo $item['img']; ?>">
+							<input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+							<input type="hidden" name="page" value="7">
+							<button type="submit" name="submit" value="modify_category">Modifier</button>
+							<button type="submit" name="submit" value="remove_category">Supprimer</button>
+						</form>
+					<?php
+				}
+				?>
+				<br>
+				<form action="index.php" method="POST" class="admin_form">
+					<input placeholder="Titre" type="text" name="category">
+					<input placeholder="Priorité" type="text" name="priority">
+					<input placeholder="Parent" type="text" name="parent">
+					<input placeholder="Lien image" type="text" name="img">
+					<input type="hidden" name="page" value="7">
+					<button type="submit" name="submit" value="add_article">Ajouter</button>
+				</form>
+			</div>
+		</div>
+		<?php
+	}
+	else
+		echo "<h1>You are not admin !</h1>"
+?>
